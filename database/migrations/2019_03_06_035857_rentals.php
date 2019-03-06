342dafb50e51b8fb56a26cbd44a836f782da02a2
jinkiyukianesa <jinkiyukianesa@gmail.com>
2019-03-06 11:13:13 +0700

    laravel

---
+++ database/migrations/2019_03_06_035857_rentals.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Rentals extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rentals', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rentals', function (Blueprint $table) {
+            //
+        });
+    }
+}
